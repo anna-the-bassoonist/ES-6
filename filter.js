35c82517f9f52a00b0953fd9321775ea90f7b19b
Anna Toeplitz <anna.toeplitz@blue-tomato.com>
2019-10-08 13:41:07 +0200

    map

---
+++ filter.js
@@ -0,0 +1,43 @@
+const products = [
+    {name: 'ipad', category: 'devices',number:20, price: 200},
+    {name: 'sony 3d', category: 'tv',number:0, price: 400},
+    {name: 'E256', category: 'blender',number:3, price: 50},
+    {name: 'super vision', category: 'tv',number:7, price: 500},
+    {name: 'iphone 7', category: 'phones',number:40, price: 2}
+];
+
+const result = products.filter(function(product){
+    return product.category === 'tv';
+});
+console.log(result);
+
+const otherResult = products.filter(function(product){
+    return product.category === 'tv' && product.number > 0 && product.price > 200;
+})
+console.log(otherResult);
+
+const channels = [
+    {name:'HBO',premium:true},
+    {name:'LIFE',premium:false},
+    {name:'Max',premium:true},
+    {name:'Cooking channel',premium:false},
+    {name:'WOBI',premium:false}
+ ];
+ const premiumChan = channels.filter(channel => channel.premium);
+ console.log(premiumChan);
+
+ const user = {
+    name:'Francis',
+    premium:true,
+    premiumChannels:function(channelsArray){
+        // GET THE PREMIUM CHANNELS IS "PREMIUM" IS true
+        return channelsArray.filter(chan => chan.premium);
+    },
+    channels:function(){
+        // GET THE NON-PREMIUM CHANNELS
+    }
+ }
+ console.log(user.premiumChannels(channels));
+ console.log(user.channels());
+
+
