4a27f343021c6ecfda43afd6c15990de1e643d22
Anna Toeplitz <anna.toeplitz@blue-tomato.com>
2019-10-09 14:23:14 +0200

    folder with files

--- js-2.js
+++
@@ -1,9 +0,0 @@
-var divs = document.getElementsByTagName('div');
-
-console.log(divs);
-
-for(let i = 0; i < divs.length; i++){
-    divs[i].addEventListener('click', function(){
-        console.log('you clicked div #' + i);
-    })
-}
