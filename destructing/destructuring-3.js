700c7a7904107f250d0c5f0cb53a8911616b0dba
Anna Toeplitz <anna.toeplitz@blue-tomato.com>
2019-10-10 12:59:55 +0200

    destructuring done

---
+++ destructing/destructuring-3.js
@@ -0,0 +1,23 @@
+const users = [
+    {name:'Francis', lastname: 'Jones', age: 26},
+    {name:'Lusia', lastname: 'Monon', age: 26},
+    {name:'Aga', lastname: 'Bones', age: 26}
+];
+//ES5
+// const lastname = users[0].lastname;
+
+const [{lastname}] = users;
+console.log(lastname);
+
+const otherUsers = {
+    names:['Francis','Ana','Ben']
+};
+
+
+const {names} = otherUsers;
+console.log(names);
+
+const {names:[name1,name2]} = otherUsers; 
+// console.log(name1);
+console.log(name2);
+
---
+++ destructing/destructuring-4.js
@@ -0,0 +1,13 @@
+function createCar({color,brand,model, type, year}){
+
+}
+
+const car = {
+    brand: 'Ford',
+    model: 'Focus',
+    color: 'red',
+    year: 2017,
+    type: 'fast'
+}
+
+createCar(car);
